2b5a92460577e80cb1ed62ed59da31f9f5d29b9d
prashant2071 <pbt9840@gmail.com>
2022-03-06 23:28:19 +0545

    updated!

--- config/config.nosql/database.js
+++ config/config.nosql/database.js
@@ -1,17 +1,20 @@
-const mongodb =require('mongodb')
-const mongoose =require('mongoose');
-const logger = require('../logger');
-const oid = mongodb.ObjectId;
-const {DB} =require('./../envCrediantials');
-// const PORT =require('./../envCrediantials').DB.PORT;
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
+const logger = require("../logger");
+const { DB } = require("./../envCrediantials");
 
-mongodb://localhost:27017
-mongoose.connect(`${DB.TYPE}://${DB.HOST}:27017/${DB.NAME}`,{useUnifiedTopology:true,
-    useNewUrlParser:true}).then(result=>{
-        console.log('DB Database connected! ✅ ✅');
-        console.log('----------------------------------');
-    }).catch(err=>{
-        console.error('failed to connect to mongo Database   ❌ ❌');
-        console.log('---------------------------------------------')
-        logger.error(err);
-    })
+// mongodb://localhost:27017
+ mongoose
+  .connect(`${DB.TYPE}://${DB.HOST}:27017/${DB.NAME}`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((result) => {
+    console.log("DB Database connected! ✅ ✅");
+    console.log("----------------------------------");
+  })
+  .catch((err) => {
+    console.error("failed to connect to mongo Database   ❌ ❌");
+    console.log("---------------------------------------------");
+    logger.error(err);
+  });
--- routes/mongoapiRoute.js
+++ routes/mongoapiRoute.js
@@ -2,7 +2,6 @@ const express =require ('express')
 const app =express();
 const productRoute =require('./../src/nosqlRoute/products/routes/productRoute')
 
-console.log('-----------mongo route--------------')
 app.use('/products',productRoute)
 
 module.exports=app;
